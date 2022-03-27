2b2a4d4b77f250e0e85b180ca1bfa2daba52c8e5
NativeAmerican315 <45272908+NativeAmerican315@users.noreply.github.com>
2022-03-27 17:24:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-#imports
 import discord
 from discord.ext import commands
 import os, random, asyncio
 
 #variables
-token = "your bot token here" #to control the bot
-dailyCat = "your main channel id here" #the main channel the bot should post to 
-dailyCatTest = "your test server channel id here" #the test channel
-cuteCats = "directory that holds all of the cat photos" #all of the cats
+token = "your bot token here"
+dailyCat = yourMainChanelIdHere
+dailyCatTest = yourChannelIdHere
+cuteCats = "your main directory here"
+cuteCatsTest = "your directory here"
 
-#gets a random file
+#random file
 random_filename = random.choice([
-    #for every cat
-    x for x in os.listdir(cuteCats)
-    #get the file and set it to the variable
-    if os.path.isfile(os.path.join(cuteCats, x))
+    x for x in os.listdir(cuteCatsTest)
+    if os.path.isfile(os.path.join(cuteCatsTest, x))
 ])
 
-#makes the bot
 client = commands.Bot(command_prefix = ":3 ")
 
-#the actual function
+#every day do this
 async def send_daily():
-    await client.wait_until_ready() #waits until the client is done setting up
-    channel = client.get_channel(dailyCat) #gets the main channel
-    while not client.is_closed(): #when the bot is up and running
-        random_filename = random.choice([ #make a new file choice
-            x for x in os.listdir(cuteCats)
-            if os.path.isfile(os.path.join(cuteCats, x))
+    await client.wait_until_ready()
+    channel = client.get_channel(dailyCatTest)
+    while not client.is_closed():
+        random_filename = random.choice([
+            x for x in os.listdir(cuteCatsTest)
+            if os.path.isfile(os.path.join(cuteCatsTest, x))
         ])
         previous = await channel.history().flatten()
         for i in range(len(previous) + 1):
@@ -36,17 +33,24 @@ async def send_daily():
                 attachment = currentMessage.attachments
                 if attachment[0].filename == random_filename:
                     random_filename = random.choice([
-                        x for x in os.listdir(cuteCats)
-                        if os.path.isfile(os.path.join(cuteCats, x))
+                        x for x in os.listdir(cuteCatsTest)
+                        if os.path.isfile(os.path.join(cuteCatsTest, x))
                     ])
                 else:
                     break
             else:
                 continue
-        file = discord.File(cuteCats+random_filename) #get the file
-        await channel.send(file=file, content="<@&your role id here> here is your cat") #and print the message
-        print('sent daily') #make a note to the bot that it sent the daily
-        await asyncio.sleep(86400) #wait 24 hours
 
-client.loop.create_task(send_daily()) #loop the function
-client.run(token) #and run the bot.
+        file = discord.File(cuteCatsTest+random_filename)
+        fileRoot = cuteCatsTest+random_filename
+        await channel.send(file=file, content="<@&your role id here> here is your cat")
+        if os.path.exists(fileRoot):
+            os.remove(fileRoot)
+        else:
+            print("the file has either already been deleted, or it broke, and the program can't read the file.")
+        print('sent daily')
+        await asyncio.sleep(10)
+
+client.loop.create_task(send_daily()) #make it loop
+
+client.run(token) #run the bot
